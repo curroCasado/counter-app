dd5dcbfc2d1985c5b5b651a2155725a75d71b294
curro casado <currocuo9625@gmail.com>
2019-11-14 23:22:58 -0500

    his is my first commit

---
+++ build/precache-manifest.c1d5b52b5f1a056daa4fe6c74bae0125.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4a914206758e8ea168285f97cac0a452",
+    "url": "/index.html"
+  },
+  {
+    "revision": "26eaddc94c92da3a5237",
+    "url": "/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "4ce7cd19fd77e8cdd504",
+    "url": "/static/js/2.e007d3d0.chunk.js"
+  },
+  {
+    "revision": "26eaddc94c92da3a5237",
+    "url": "/static/js/main.40d34f49.chunk.js"
+  },
+  {
+    "revision": "9c3c8fadcd7c4b507e8d",
+    "url": "/static/js/runtime-main.db26aea7.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/static/media/logo.25bf045c.svg"
+  }
+]);
